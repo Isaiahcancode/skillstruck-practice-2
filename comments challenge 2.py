5a614ba4fd87f12d967afc77d8f343a6d80edd97
Isaiahcancode <ITayl715@evit.edu>
2024-08-29 09:31:51 -0700

    exported from Skill Struck

---
+++ comments challenge 2.py
@@ -0,0 +1,7 @@
+#comment goes here
+morning = input("what do you do in morning?")
+print(morning)
+afternoon = input("what do you do in morning?")
+print(afternoon)
+evening = input("what do you do in morning?")
+print(evening)
