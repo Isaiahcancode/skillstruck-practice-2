0ab6c3a561801b0613f8daaa585576457f1caaf2
Isaiahcancode <ITayl715@evit.edu>
2024-09-13 10:18:20 -0700

    exported from Skill Struck

---
+++ object oriented programming.py
@@ -0,0 +1,8 @@
+class Tree:
+
+  	def __init__(self, fruit, height, leaf, flower, seed):
+         self.fruit = fruit
+         self.height = height
+         self.leaf = leaf
+         self.flower = flower
+         self.seed = seed
