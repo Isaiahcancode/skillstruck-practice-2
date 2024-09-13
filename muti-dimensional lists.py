e0d940cdc4c3404fadd99983a8b3a13a7a00c62c
Isaiahcancode <ITayl715@evit.edu>
2024-09-13 10:18:17 -0700

    exported from Skill Struck

---
+++ muti-dimensional lists.py
@@ -0,0 +1,9 @@
+rows = ['David', 'Goliath', 'Isaiah', 'Michale']
+cols = ['hammer', 'fortune', 'Taylor', 'len']
+list = []
+for j in cols:
+    name = []
+    for i in rows:
+        col.append(i + " " + j)
+    list.append(rows)
+    print(list)
