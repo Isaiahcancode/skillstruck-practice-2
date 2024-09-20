dc6224fdf7fcc4a2f4441489ee3b5c7be5eab5ac
Isaiahcancode <ITayl715@evit.edu>
2024-09-20 10:12:13 -0700

    exported from Skill Struck

---
+++ instance challenge.py
@@ -0,0 +1,9 @@
+class Fruit:
+
+  	def __init__(self, shape, kind, size,):
+          self.shape = shape
+          self.kind = kind
+          self.size = size
+ki = Fruit("oval", "apple", "large")
+
+print(ki)
