b0cdcc1bdd0af9dae48f0e738e70930f72f169ad
Isaiahcancode <ITayl715@evit.edu>
2024-09-20 10:12:10 -0700

    exported from Skill Struck

--- w.py
+++ w.py
@@ -1,14 +1,3 @@
-def = AGE(current_year)
-
-current_year = int(input("enter a year"))
-age = (current_year - year_of_birth)
-if age >= 2:
-    print("you are " + str(age) + " years old")
-elif age == "1":
-    print("you are 1 year old")
-elif age == "0":
-    print("you will be born this very year.")
-elif age == "-1":
-    print("you will be born in 1 year.") 
-else:
-    print("you will be born in " + str(abs(age)) + " years.")
+num= 10%2
+nums= 10 / 2
+print(nums)
