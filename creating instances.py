35ea4dad88c13e86905ff2eeaecec705de1f4d00
Isaiahcancode <ITayl715@evit.edu>
2024-09-20 10:12:12 -0700

    exported from Skill Struck

---
+++ creating instances.py
@@ -0,0 +1,8 @@
+class person:
+
+  	def __init__(self, name, age, gender):
+          self.name = name
+          self.age = age
+          self.gender = gender
+p1 = person("Jasmine", "15", "Female")
+print(p1.name)
