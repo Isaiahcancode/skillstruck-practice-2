75af96a11ea3ae7a8580ec3289ece6ea09ed243c
Isaiahcancode <ITayl715@evit.edu>
2024-11-08 09:22:09 -0700

    exported from Skill Struck

---
+++ challenge instance 2.py
@@ -0,0 +1,9 @@
+class Pet:
+
+    def __init__(self, size, age, color):
+        self.size = size
+        self.age = age
+        self.color = color
+cat_1 = Pet("15", "12", "orange")
+cat_2 = Pet("10", "12", "red")
+cat_3 = Pet("11", "13", "blue")
