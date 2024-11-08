9315ca3153b6ec70633519f46d32967d6dce0535
Isaiahcancode <ITayl715@evit.edu>
2024-11-08 09:22:09 -0700

    exported from Skill Struck

--- instance challenge.py
+++ instance challenge.py
@@ -4,6 +4,12 @@ class Fruit:
           self.shape = shape
           self.kind = kind
           self.size = size
-ki = Fruit("oval", "apple", "large")
-
-print(ki)
+fruit = Fruit("oval", "apple", "large")
+fruit2 = Fruit("circle", "orange", "small")
+fruit3 = Fruit("oval", "watermelon", "larger")
+fruit4 = Fruit("Cresent", "Bannana", 
+"large")
+print(fruit.kind)
+print(fruit2.kind)
+print(fruit3.kind)
+print(fruit4.kind)
